70d9d4230b80ba5c782d17d065779f1db657c8c3
Levy A. <levyddsa@gmail.com>
2025-04-16 16:49:26 -0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            ruff
+            uv
+            poetry
+            python312
+            python312Packages.pip
+            python312Packages.virtualenv
+            python312Packages.pytest
+            python312Packages.pyperf
+          ];
+          shellHook = ''
+            virtualenv .venv
+            source .venv/bin/activate
+          '';
+        };
+      });
+}
