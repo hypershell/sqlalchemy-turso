f05878f2b24812f4fd889b24ea4235d9418cb7f7
Levy A. <levyddsa@gmail.com>
2025-04-17 13:43:17 -0300

    feat: async stuff!

---
+++ libsql_sqlalchemy/aiolibsql.py
@@ -0,0 +1,10 @@
+from libsql_sqlalchemy.libsql import SQLiteDialect_libsql
+
+
+class SQLiteDialect_aiolibsql(SQLiteDialect_libsql):
+    driver = "libsql"
+    supports_statement_cache = SQLiteDialect_libsql.supports_statement_cache
+    is_async = True
+
+
+dialect = SQLiteDialect_aiolibsql
